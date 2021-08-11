5817472267c4a332151567a48db16a10829ab570
Dakshraj Sharma <sharmarajdaksh@gmail.com>
2021-08-11 21:22:06 +0530

    Added POJOs. Closes #2

---
+++ src/main/java/com/example/petclinic/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.example.petclinic.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/example/petclinic/petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.example.petclinic.petclinic.model;
+
+public class Vet extends Person {
+}
