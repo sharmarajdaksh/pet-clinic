f7c43cba004dd637a9a263ccfc58a17daeec1777
Dakshraj Sharma <sharmarajdaksh@gmail.com>
2021-08-14 20:18:28 +0530

    Added vet speciality model. Closes #26.

--- pet-clinic-data/src/main/java/com/example/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Vet.java
@@ -1,4 +1,16 @@
 package com.example.petclinic.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Vet extends Person {
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
+
+    private Set<Speciality> specialities = new HashSet<>();
 }
